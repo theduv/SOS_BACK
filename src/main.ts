9a74a8adb144c6d6875bf7219936dbdd9e092176
Théo <tduval@student.42.fr>
2022-02-10 19:25:44 +0100

    added ssl

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const fs = require('fs')
+const keyFile  = fs.readFileSync('../key.pem');
+const certFile  = fs.readFileSync('../cert.pem');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: keyFile,
+      cert: certFile
+    }
+  });
   await app.listen(3002);
 }
 bootstrap();
